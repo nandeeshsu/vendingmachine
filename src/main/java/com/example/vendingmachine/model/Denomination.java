947019a66cb9e42157f2660029f44c0a7834b1b3
Nandeesh Subbegowda <nandeesh.subbegowda@bt.com>
2021-05-23 17:41:28 +0100

    Added enum for the denomination

---
+++ src/main/java/com/example/vendingmachine/model/Denomination.java
@@ -0,0 +1,22 @@
+package com.example.vendingmachine.model;
+
+public enum Denomination {
+    ONE_PENCE(1),
+    TWO_PENCE(2),
+    FIVE_PENCE(5),
+    TEN_PENCE(10),
+    TWENTY_PENCE(20),
+    FIFTY_PENCE(50),
+    ONE_POUND(1),
+    TWO_POUND(2),;
+
+    private final Integer value;
+
+    private Denomination(Integer value) {
+        this.value = value;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+}
